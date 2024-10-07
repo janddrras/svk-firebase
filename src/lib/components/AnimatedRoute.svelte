5e6a4de21c7b82d1b87ea568ea91ef331249c7d4
janddrras <janddrras@gmail.com>
2024-10-07 19:04:57 +0300

    animated login component

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { fly} from "svelte/transition";
+    import { page } from "$app/stores";
+		import { quadOut } from "svelte/easing";
+</script>
+
+{#key $page.url}
+  <div in:fly={{  x:'-10%', duration: 500, easing: quadOut }}>
+    <slot  />
+  </div>
+{/key}
