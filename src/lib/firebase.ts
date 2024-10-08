d6ac21b64e2333f5e45d533a0bef1edff553fb70
janddrras <janddrras@gmail.com>
2024-10-08 10:57:08 +0300

    Auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,47 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { firebaseConfig } from './config'
+import { writable } from 'svelte/store'
 
 // Your web app's Firebase configuration
 // Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
+
+// export const analytics = getAnalytics(app)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 export const storage = getStorage(app)
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+	let unsubscribe: () => void
+
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser')
+		const { subscribe } = writable<User | null>(null)
+		return {
+			subscribe
+		}
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user)
+		})
+
+		return () => unsubscribe()
+	})
+
+	return {
+		subscribe
+	}
+}
+
+export const user = userStore()
